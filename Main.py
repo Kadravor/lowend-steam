f0921e56680a24eb31f65a6763198ec0a0ceb22a
Kadravor <Kadravor@hotmail.fr>
2020-04-15 17:27:13 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,16 @@
-import steam
+from steam import *
 
 id_url = 'https://steamcommunity.com/id/Kadravor'
-player_id = SteamId.from_url(id_url)
+api = WebAPI(key="194A600C4357E8560BCCCA2BA69377BA")
+account_id = steam.steamid.steam64_from_url(id_url)
+
+print(account_id)
+
+
+Owned_games = api.IPlayerService.GetOwnedGames(steamid=account_id, format='json', include_appinfo=1, include_played_free_games=1, appids_filter=0)
+Owned_games['response']['games'][1]['name']
+
+game_list = []
+for n in range(0,Owned_games['response']['game_count']):
+    game_list.append(Owned_games['response']['games'][n]['name'])
+game_list
